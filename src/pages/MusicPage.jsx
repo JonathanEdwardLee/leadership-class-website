07b10221516d635111b37ff3ab0381449c1428b4
Jonathan Edward Lee <94152189+JonathanEdwardLee@users.noreply.github.com>
2024-05-31 00:43:12 -0500

    MusicPage Update

--- src/pages/MusicPage.jsx
+++ src/pages/MusicPage.jsx
@@ -9,56 +9,57 @@ const MusicPage = () => {
       <Tabs defaultActiveKey="bandcamp" id="music-tabs" className="mb-3 custom-tabs">
         <Tab eventKey="bandcamp" title="Bandcamp" className="custom-tab">
           <div className="embed-container">
-            <iframe 
-              style={{ border: '0', width: '100%', height: '472px' }} 
-              src="https://bandcamp.com/EmbeddedPlayer/album=1843728153/size=large/bgcol=333333/linkcol=e99708/artwork=none/transparent=true/" 
+            <iframe
+              style={{ border: 0, width: '350px', height: '786px' }}
+              src="https://bandcamp.com/EmbeddedPlayer/album=1556870638/size=large/bgcol=333333/linkcol=e32c14/transparent=true/"
               seamless
-              title="Bandcamp">
-              <a href="https://leadershipclass.bandcamp.com/album/demos-we-are-recording-ourselves-for-cool-people-to-listen-to">
-                Demos We Are Recording Ourselves For Cool People To Listen To by Leadership Class
+              title="Bandcamp"
+            >
+              <a href="https://leadershipclass.bandcamp.com/album/live-rehearsal-at-the-fungeon">
+                Live Rehearsal At The Fungeon by Leadership Class
               </a>
             </iframe>
           </div>
         </Tab>
         <Tab eventKey="spotify" title="Spotify" className="custom-tab">
           <div className="embed-container">
-            <iframe 
-              style={{ borderRadius: '12px' }} 
-              src="https://open.spotify.com/embed/artist/75gcjhF5ZNIkfZhh1xqbQ2?utm_source=generator&theme=0" 
-              width="100%" 
-              height="352" 
-              frameBorder="0" 
-              allowFullScreen 
-              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+            <iframe
+              style={{ borderRadius: '12px' }}
+              src="https://open.spotify.com/embed/artist/75gcjhF5ZNIkfZhh1xqbQ2?utm_source=generator&theme=0"
+              width="100%"
+              height="352"
+              frameBorder="0"
+              allowFullScreen
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
               loading="lazy"
-              title="Spotify">
-            </iframe>
+              title="Spotify"
+            ></iframe>
           </div>
         </Tab>
         <Tab eventKey="amazon" title="Amazon Music" className="custom-tab">
           <div className="embed-container">
-            <iframe 
-              id='AmazonMusicEmbedB09QNYXHHG' 
-              src='https://music.amazon.com/embed/B09QNYXHHG/?id=h02geHzt3M&marketplaceId=ATVPDKIKX0DER&musicTerritory=US' 
-              width='100%' 
-              height='352px' 
-              frameBorder='0' 
+            <iframe
+              id="AmazonMusicEmbedB09QNYXHHG"
+              src="https://music.amazon.com/embed/B09QNYXHHG/?id=h02geHzt3M&marketplaceId=ATVPDKIKX0DER&musicTerritory=US"
+              width="100%"
+              height="352px"
+              frameBorder="0"
               style={{ borderRadius: '20px' }}
-              title="Amazon Music">
-            </iframe>
+              title="Amazon Music"
+            ></iframe>
           </div>
         </Tab>
         <Tab eventKey="apple" title="Apple Music" className="custom-tab">
           <div className="embed-container">
-            <iframe 
-              allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" 
-              frameBorder="0" 
-              height="450" 
-              style={{ width: '100%', maxWidth: '660px', overflow: 'hidden', borderRadius: '10px' }} 
-              sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" 
+            <iframe
+              allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+              frameBorder="0"
+              height="450"
+              style={{ width: '100%', maxWidth: '660px', overflow: 'hidden', borderRadius: '10px' }}
+              sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
               src="https://embed.music.apple.com/us/album/here-we-go-single/1659586681"
-              title="Apple Music">
-            </iframe>
+              title="Apple Music"
+            ></iframe>
           </div>
         </Tab>
       </Tabs>
